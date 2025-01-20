d31a55990242eaa87d448457a86c7a3206bcc9c2
Alex Avila <alex.avila.09@outlook.com>
2025-01-20 04:33:05 -0700

    feat: set up some base colors, basic layout and basic css

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,21 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        "app-blue": {
+          DEFAULT: "#4EA8DE",
+          dark: "#1E6F9F",
+        },
+        "app-purple": {
+          DEFAULT: "#8284FA",
+          dark: "#5357c2",
+        },
+        "app-gray": {
+          200: "#808080",
+          300: "#333333",
+          350: "#262626",
+          400: "#1a1a1a",
+          500: "#0d0d0d",
+        },
       },
     },
   },
